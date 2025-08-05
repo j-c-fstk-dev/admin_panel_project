38081fe21618e6f05b14bb067049c9a2b3bd6f09
j-c-fstk-dev <dev.jorge.c@gmail.com>
2025-08-05 01:46:15 -0300

    feat: adding index.tsx to frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+// frontend/src/pages/index.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Painel Administrativo!</h1>
+      <p>A aplicação está funcionando corretamente.</p>
+    </div>
+  );
+};
+
+export default Home;
